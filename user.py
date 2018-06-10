9e50ed884a0d2e129bb598190414dd04799a1282
Eddie956 <mutugieddie3@gmail.com>
2018-06-11 01:43:34 +0300

    this willl commit class user to initialize the method that helps us define our propeties in user.py

--- user.py
+++ user.py
@@ -17,73 +17,73 @@ class User:
 		self.email = email
 		self.password = password
 
-	def save_account(self):
-		"""
-		save_user method saves user objects into user_list
-		"""
+# 	def save_account(self):
+# 		"""
+# 		save_user method saves user objects into user_list
+# 		"""
 
-		User.user_list.append(self)
+# 		User.user_list.append(self)
 		
-class Credential:
-	credential_list = []
-	user_credential_list = []
+# class Credential:
+# 	credential_list = []
+# 	user_credential_list = []
 
-	@classmethod
-	def check_user(cls,first_name,password):
-		'''
-		Method that checks if the name and password entered match entries in the users_list
-		'''
-		current_user = ''
-		for user in User.users_list:
-			if (user.first_name == first_name and user.password == password):
-				current_user = user.first_name
-		return current_user
-
-
-	def __init__(self,user_name,email, acc, password):
-		self.user_name = user_name
-		self.email = email
-		self.password = password
-
-	def save_credential(self):
-		Credential.credential_list.append(self)#function that saves the credential list
-
-	def generate_password():
-		generate_password=  "".join(choice(characters) for x in range(randint(8, 16)))
-		return generate_password
-
-	def delete_credential(self):
-		Credential.credential_list.append(self)#function that delete the credential list
+# 	@classmethod
+# 	def check_user(cls,first_name,password):
+# 		'''
+# 		Method that checks if the name and password entered match entries in the users_list
+# 		'''
+# 		current_user = ''
+# 		for user in User.users_list:
+# 			if (user.first_name == first_name and user.password == password):
+# 				current_user = user.first_name
+# 		return current_user
 
 
+# 	def __init__(self,user_name,email, acc, password):
+# 		self.user_name = user_name
+# 		self.email = email
+# 		self.password = password
 
-	@classmethod
-	def find_by_user_name(cls,user_name):
+# 	def save_credential(self):
+# 		Credential.credential_list.append(self)#function that saves the credential list
 
-		for credential in cls.credential_list:
-			if credential.user_name == user_name:
-				return credential
+# 	def generate_password():
+# 		generate_password=  "".join(choice(characters) for x in range(randint(8, 16)))
+# 		return generate_password
 
-	@classmethod
-	def credential_exists(cls,user_name):
-		"""
-		method that checks if a username exists
-		"""
-		for credential in cls.credential_list:
-			if credential.user_name == user_name:
-				return True
-
-		return False
+# 	def delete_credential(self):
+# 		Credential.credential_list.append(self)#function that delete the credential list
 
 
-	@classmethod
-	def display_credential(cls):
-		"""
-		method that returns the credential list
-		"""
-		return cls.credential_list
 
-	@classmethod
-	def copy_email(cls,user_name):
-		credential_found = Credential.find_by_user_name(user_name)
-		return pyperclip.copy(credential_found.email)
+# 	@classmethod
+# 	def find_by_user_name(cls,user_name):
+
+# 		for credential in cls.credential_list:
+# 			if credential.user_name == user_name:
+# 				return credential
+
+# 	@classmethod
+# 	def credential_exists(cls,user_name):
+# 		"""
+# 		method that checks if a username exists
+# 		"""
+# 		for credential in cls.credential_list:
+# 			if credential.user_name == user_name:
+# 				return True
+
+# 		return False
+
+
+# 	@classmethod
+# 	def display_credential(cls):
+# 		"""
+# 		method that returns the credential list
+# 		"""
+# 		return cls.credential_list
+
+# 	@classmethod
+# 	def copy_email(cls,user_name):
+# 		credential_found = Credential.find_by_user_name(user_name)
+# 		return pyperclip.copy(credential_found.email)
